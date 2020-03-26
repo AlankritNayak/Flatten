3a0b7ebf4d1099d45f3fe828451677fd07739ed0
DritonUkic <DritonUkic@users.noreply.github.com>
2020-03-26 13:02:38 +0100

    Revert "update typeorm type"
    This reverts commit 8deb9a35ddc1995eebc102e70b27a1788e4ecba4.

--- flatten-backend/src/app.module.ts
+++ flatten-backend/src/app.module.ts
@@ -8,7 +8,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
+      type: 'mysql',
       host: 'localhost',
       port: 3306,
       username: 'root',
