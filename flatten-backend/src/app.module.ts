3929c37af1790786e59616d0b1187fa954321f6c
DritonUkic <DritonUkic@users.noreply.github.com>
2020-03-26 13:04:49 +0100

    Revert "update typeorm type"
    This reverts commit 8deb9a35ddc1995eebc102e70b27a1788e4ecba4.

--- flatten-backend/src/app.module.ts
+++ flatten-backend/src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+<<<<<<< HEAD
 import {QrCodeModule} from "./qr-code/qr-code.module";
 
 @Module({
   imports: [QrCodeModule],
+=======
+import { QrCodeModule } from "./qr-code/qr-code.module";
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+    QrCodeModule
+  ],
+>>>>>>> parent of b51411b... Merge branch 'dev-backend-Driton' of https://github.com/Platri/Flatten into dev-backend-Driton
   controllers: [AppController],
   providers: [AppService],
 })
